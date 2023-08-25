b2e31565935022e8c505c8a73b0fd266f65e5a32
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2023-08-25 18:05:03 +0200

    Added a script to generate the index json from what is found in the assets folder.

---
+++ generate_index_json.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+URL = "https://github.com/SillyTavern/SillyTavern-Content/raw/main/"
+ASSETS_FOLDER = "assets/"
+OUTPUT_JSON = "index.json"
+
+
+if __name__ == "__main__":
+    assets_json = []
+    for path, subdirs, files in os.walk(ASSETS_FOLDER):
+        for name in files:
+            type = path[len(ASSETS_FOLDER):]
+            id = name
+            url = URL + ASSETS_FOLDER + type + "/" + name
+            entry = {"type":type,"id":id,"url":url}
+            print("Creating entry:", entry)
+            assets_json.append(entry)
+
+    with open(OUTPUT_JSON, "w") as outfile:
+        outfile.write(json.dumps(assets_json,indent=4))
+    print("Done.")
